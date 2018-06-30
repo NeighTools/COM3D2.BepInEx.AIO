62d1d4b9f8a168590534140fea3475afbe23130c
denikson <geoffrey.hoooooorse@gmail.com>
2018-06-30 17:58:43 +0300

    Add Sybaris 2 migrator

---
+++ SybarisMigrator/SybarisMigrator/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SybarisMigrator
+{
+    public static class Program
+    {
+        private static readonly string[] FilesToRemove =
+        {
+                "Sybaris.Loader.dll", "COM3D2.UnityInjector.Patcher.dll", "Mono.Cecil.dll", "ExIni.dll", "opengl32.dll", "UnityInjector.dll"
+        };
+
+        public static void Main(string[] args)
+        {
+            if (IsPatched())
+                PrintMigrated();
+            else
+                DoMigrate();
+        }
+
+        private static void WriteLine(ConsoleColor col, string message)
+        {
+            ConsoleColor old = Console.ForegroundColor;
+            Console.ForegroundColor = col;
+            Console.WriteLine(message);
+            Console.ForegroundColor = old;
+        }
+
+        private static void Write(ConsoleColor col, string message)
+        {
+            ConsoleColor old = Console.ForegroundColor;
+            Console.ForegroundColor = col;
+            Console.Write(message);
+            Console.ForegroundColor = old;
+        }
+
+        private static bool IsPatched()
+        {
+            return File.Exists("sybaris_migrator.lock");
+        }
+
+        private static void PrintMigrated()
+        {
+            Console.Clear();
+
+            WriteLine(ConsoleColor.Green, new string('=', Console.WindowWidth - 2));
+            WriteLine(ConsoleColor.Green, "Info");
+            WriteLine(ConsoleColor.Green, new string('=', Console.WindowWidth - 2));
+            Console.WriteLine();
+            Console.WriteLine("Migration is complete!");
+            Console.WriteLine("Sybaris Migrator detects that the migration is complete. You don't have to run the migrator again.");
+            Console.WriteLine("You can run the game normally.");
+            Console.WriteLine();
+            Console.WriteLine();
+            WriteLine(ConsoleColor.DarkGray, "Hint: If you want to rerun the migrator, remove sybaris_migrator.lock file!");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close...");
+            Console.ReadKey(true);
+        }
+
+        private static void DoMigrate()
+        {
+            Console.Clear();
+
+            WriteLine(ConsoleColor.Green, new string('=', Console.WindowWidth - 2));
+            WriteLine(ConsoleColor.Green, "Sybaris Migrator");
+            WriteLine(ConsoleColor.Green, new string('=', Console.WindowWidth - 2));
+            Console.WriteLine();
+
+            Console.WriteLine("Welcome to Sybaris Migrator!");
+            Console.WriteLine("This tool will automatically migrate your Sybaris 2 installation to BepInEx.");
+            Console.WriteLine();
+            Console.WriteLine("NOTE: Please run this EXE from the game's main directory if it's not there yet.");
+            Console.WriteLine();
+            Write(ConsoleColor.Red, "WARNING: ");
+            Console.WriteLine("Do not run this tool if you don't have Sybaris 2 installed!");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to begin migration...");
+            Console.ReadKey(true);
+
+            Console.Clear();
+            WriteLine(ConsoleColor.Green, new string('=', Console.WindowWidth - 2));
+            WriteLine(ConsoleColor.Green, "Step 1: Detecting Sybaris files");
+            WriteLine(ConsoleColor.Green, new string('=', Console.WindowWidth - 2));
+            Console.WriteLine();
+
+            Console.WriteLine("First, the migrator will try to find files that will conflict with BepInEx.");
+            Console.WriteLine("The migrator WILL NOT DELETE anything!");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to search for files...");
+            Console.ReadKey(true);
+
+            Console.Clear();
+
+            Console.WriteLine("Searching for Sybaris files. This will take a moment.");
+
+            var invalidFiles = new HashSet<string>(FilesToRemove, StringComparer.InvariantCultureIgnoreCase);
+            var filesToDelete = new HashSet<string>();
+            foreach (string file in Directory.EnumerateFiles(".", "*", SearchOption.AllDirectories))
+            {
+                string path = Path.GetDirectoryName(file).ToLowerInvariant();
+                string name = Path.GetFileName(file);
+
+                if (path.Contains("bepinex") || path.Contains("sybaris_old"))
+                    continue;
+
+                if (!invalidFiles.Contains(name))
+                    continue;
+                Console.WriteLine($"Found {file}");
+                filesToDelete.Add(file);
+            }
+
+            Console.Clear();
+            WriteLine(ConsoleColor.Green, new string('=', Console.WindowWidth - 2));
+            WriteLine(ConsoleColor.Green, "Step 2: Moving the files");
+            WriteLine(ConsoleColor.Green, new string('=', Console.WindowWidth - 2));
+            Console.WriteLine();
+
+            Console.WriteLine("The migrator found the following conflicting files:");
+            foreach (string s in filesToDelete)
+                Console.WriteLine($"* {s}");
+            Console.WriteLine();
+
+            Console.Write("Migrator will now move these files to ");
+            Write(ConsoleColor.Cyan, "sybaris_old");
+            Console.WriteLine(" folder.");
+            Console.WriteLine();
+
+            WriteLine(ConsoleColor.Yellow,
+                      "The next operation is permanent. To reverse it you will have to manually move the files out of sybaris_old.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to migrate");
+            Console.WriteLine("Press ESC to exit");
+
+            ConsoleKeyInfo info;
+            do
+            {
+                info = Console.ReadKey(true);
+            } while (info.Key != ConsoleKey.Enter && info.Key != ConsoleKey.Escape);
+
+            if (info.Key == ConsoleKey.Escape)
+                Environment.Exit(0);
+
+            if (!Directory.Exists("sybaris_old"))
+                Directory.CreateDirectory("sybaris_old");
+
+            foreach (string file in filesToDelete)
+            {
+                string dest = Path.GetFullPath(Path.Combine("sybaris_old", file));
+                Directory.CreateDirectory(Path.GetDirectoryName(dest));
+                File.Move(Path.GetFullPath(file), dest);
+            }
+
+            File.WriteAllText("sybaris_migrator.lock", "REMOVE ME IF YOU WANT TO RE-RUN SYBARIS MIGRATOR");
+
+            PrintMigrated();
+        }
+    }
+}
--- docs/README_en.md
+++ docs/README_en.md
@@ -14,7 +14,7 @@ The package contains
 * BepInEx.UnityInjectorLoader ([GitHub](https://github.com/BepInEx/BepInEx.UnityInjectorLoader)) -- a plug-in that emulates UnityInjector in BepInEx
 * SybarisMigrator ([GitHub](https://github.com/NeighTools/COM3D2.BepInEx.AIO)) -- a tool that automatically migrates Sybaris to BepInEx
 
-This package is optimized for COM3D2 and Sybaris 2, but it can be used on CM3D2 as well.
+This package is optimized for COM3D2 and Sybaris 2, but it can be used on CM3D2 as well (as long as it uses Sybaris 2).
 
 ## How to install
 
@@ -22,57 +22,13 @@ This package is optimized for COM3D2 and Sybaris 2, but it can be used on CM3D2 
 2. Run `SybarisMigrator.exe`
 3. Follow the migration guide in the opened window
 
+## Restoring back to Sybaris 2
+
+1. Remove all the files/folders you copied
+2. Move all folders from `sybaris_old` folder into the game's root folder. Overwrite if needed.
+3. Remove `sybaris_migrator.lock`
 
 ## Credits
 
-Licence for BepInEx, SybarisLoader and UnityInjectorLoader:
-
-```
-MIT License
-
-Copyright (c) 2018 Bepis
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-```
-
-Licence for SybarisMigrator
-
-```
-MIT License
-
-Copyright (c) 2018 Geoffrey Horsington
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-```
+BepInEx, UnityInjecorLoader and SybarisLoader are licensed to Bepis under the MIT licence.  
+SybarisMigrator is licensed to Geoffrey Horsington under the MIT licence.
